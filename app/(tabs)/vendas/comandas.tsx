18ed38a275d0b6bd316c716fa35c2de807e4dcb7
Andrey Mendonca <andrey.mmendonca@gmail.com>
2025-01-28 17:41:48 -0300

    Telas de comandas criada

--- app/(tabs)/vendas/comandas.tsx
+++ app/(tabs)/vendas/comandas.tsx
@@ -1,12 +1,30 @@
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, ScrollView, Text } from "react-native";
 import { Header } from "../../../components/Header";
 import { BuscaECadastro } from "../../../components/BuscaECadastro";
+import { ComandaAberta } from "../../../components/ComandaAberta";
 
 export default function Screen(){
     return(
-        <SafeAreaView>
+        <SafeAreaView className="flex-1">
             <Header nome="Comandas" voltar={true}/>
             <BuscaECadastro rota=""/>
+            <ScrollView className="m-2">
+                <ComandaAberta/>
+                <ComandaAberta/>
+                <ComandaAberta/>
+                <ComandaAberta/>
+                <ComandaAberta/>
+                <ComandaAberta/>
+                <ComandaAberta/>
+                <ComandaAberta/>
+                <ComandaAberta/>
+                <ComandaAberta/>
+                <ComandaAberta/>
+                <ComandaAberta/>
+                <ComandaAberta/>
+                <ComandaAberta/>
+                <ComandaAberta/>
+            </ScrollView>
         </SafeAreaView>
     )
 }
---
+++ components/ComandaAberta.tsx
@@ -0,0 +1,25 @@
+import { Pressable, Text, View } from "react-native"
+import Icon from '@expo/vector-icons/FontAwesome6';
+
+export const ComandaAberta = () =>{
+    return(
+        <Pressable className="bg-green-600 m-2 rounded-xl h-20 items-center justify-between flex-row">
+            <View className="flex-row items-center ml-6">
+                <View className="items-center mr-3">
+                    <Text className="text-sm font-medium mr-2">Identifi</Text>
+                    <Text className="text-sm font-medium mr-2">cacao: </Text>
+                </View>
+                
+                <Text className="text-2xl font-medium mr-5">Andrey Marques</Text>
+                <View>
+                    <Icon name="circle-plus" size={28} color="white" />
+                    <Text className="text-white text-sm">Add</Text>
+                </View>
+            </View>
+            <Pressable className="items-center justify-center mr-3">
+                <Icon name="hand-holding-dollar" size={28} color="white" />
+                <Text className="text-white text-sm">Fechar</Text>
+            </Pressable>
+        </Pressable>
+    )
+}
